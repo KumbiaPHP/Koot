ac30ba25c18855486e215638bd197c4dc0f696d8
Miguel Ángel <jorquera11@gmail.com>
2020-05-10 11:17:34 +0200

    clase abstract LiteRecord

---
+++ app/libs/lite_record.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @see KumbiaActiveRecord
+ */
+require_once CORE_PATH.'libs/kumbia_active_record/kumbia_active_record.php';
+
+/**
+ * LiteRecord
+ *
+ * Clase abstracta LiteRecord
+ *
+ */
+abstract class LiteRecord extends \Kumbia\ActiveRecord\LiteRecord
+{
+
+}
