e6cf0acbd1b0110bf97e1a65159b79b12b7ce41a
Henry Stivens Adarme M <henry.stivens@gmail.com>
2020-05-17 11:38:04 -0500

    Improves create() and edit() methods

--- app/libs/crud_controller.php
+++ app/libs/crud_controller.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * Parent Controller to manage the database
+ * Parent Controller to manage the database with the
+ * new ActiveRecord
  */
 class CrudController extends AdminController
 {
-    
+
     /** @var string Nombre del modelo en CamelCase */
     public $model = '';
 
@@ -22,33 +23,39 @@ class CrudController extends AdminController
     public function create()
     {
         // It is verified if the data has been sent via POST
-        if (Input::hasPost($this->model)) {
+        if (Input::hasPost('data')) {
             $obj = new Users;
             // Try to save the user
-            if ($obj->create(Input::post($this->model))) {
+            if ($obj->create(Input::post('data'))) {
                 // Success message and return to the list
                 Flash::valid(_('Record created '));
-                return Redirect::to();
+                return Redirect::toAction('page');
             }
             // If it fails the data is persistent in the form
-            $this->data = Input::post($this->model);
+            $this->data = Input::post('data');
         }
     }
 
     public function edit(int $id)
     {
+        View::select('create');
         // Load the data from database
         $this->data = $this->model::get($id);
+        //If not exist
+        if (!$this->data) {
+            Flash::warning(_('Record not found'));
+            return Redirect::toAction('page');
+        }
         // It is verified if the data has been sent via POST
-        if (Input::hasPost($this->model)) {
+        if (Input::hasPost('data')) {
             // Try to save changes
-            if ($this->data->update(Input::post($this->model))) {
+            if ($this->data->update(Input::post('data'))) {
                 // Success message and return to the list
                 Flash::valid(_('Record updated'));
-                return Redirect::to();
+                return Redirect::toAction('page');
             }
             // If it fails the data is persistent in the form
-            $this->data = Input::post($this->model);
+            $this->data = Input::post('data');
         }
     }
 }
