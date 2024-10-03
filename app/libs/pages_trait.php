2cd2a03bb77c1e6ffe8a3c3c9f7248195c732877
Joan Miquel <joanhey@kumbiaphp.com>
2024-10-03 03:50:01 +0200

    Clean pages trait

--- app/libs/pages_trait.php
+++ app/libs/pages_trait.php
@@ -6,8 +6,6 @@ trait PagesTrait
 
     public function __call($name, $params)
     {
-        //array_unshift($params, $name);
-        //exit(var_dump($name, $params));
         View::select('pages/' . implode('/', [$name, ...$params]));
     }
 }
